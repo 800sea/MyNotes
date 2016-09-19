0db862511fb9c7506720d4541997339fc117fa76
sea <493069076@qq.com>
2016-09-19 09:39:55 +0800

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by sea on 16/9/19.
+//  Copyright © 2016年 sea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
