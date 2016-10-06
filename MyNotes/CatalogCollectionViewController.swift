5d354d2144abc2c2b89ba103dbbb71e0544359d9
sea <493069076@qq.com>
2016-10-06 19:51:15 +0800

    fix

--- MyNotes/CatalogCollectionViewController.swift
+++ MyNotes/CatalogCollectionViewController.swift
@@ -174,11 +174,9 @@ class CatalogCollectionViewController: UICollectionViewController,UIGestureRecog
     @IBAction func deleteCell(_ sender: AnyObject) {
         if self.cellStating == .nomale {
             self.cellStating = .delete
-           // (sender as! UIButton).titleLabel?.text = "列表"
             (sender as! UIButton).setTitle("列表", for: .normal)
         }else{
             self.cellStating = .nomale
-           //  (sender as! UIButton).titleLabel?.text = "删除"
             (sender as! UIButton).setTitle("删除", for: .normal)
         }
         
